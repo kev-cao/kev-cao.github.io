2632f424e50560887f9305e57e84f5b27b60c0bb
Kevin Cao <kcao1998@gmail.com>
2020-10-25 02:28:04 -0400

    terminal completed

---
+++ js/terminal.js
@@ -0,0 +1,106 @@
+// terminal.js
+let hasCleared = false;
+
+const helpMap = {
+  'whoami': 'Show my name.',
+  'id': 'Print my major information.',
+  'pwd': 'Print name of current college.',
+  'ps': 'Report a snapshot of work experience.',
+  'ls': 'List links to notable Github projects.',
+  'locale': 'Get known programming languages.',
+  'clear': 'Clear the terminal.',
+  'help': 'Show this help dialog.'
+}
+
+let tableContents = '';
+for (let command in helpMap) {
+  tableContents += '<tr>' +
+    '<td style="width: 10em; vertical-align: top;"><b>' + command + ':</b></td>' +
+    '<td>' + helpMap[command] + '</td>' +
+    '</tr>';
+}
+
+const projects = {
+  'NEAT AI for Dino Run': 'https://github.com/defCoding/NEAT-dino-run',
+  'Teaching Helper Discord Bot': 'https://github.com/defCoding/bot-TA-discord',
+  'Sorting Algorithms Visualizer': 'https://github.com/defCoding/sort-visualizer'
+}
+
+$('#terminal-container').terminal({
+  help: function() {
+    const table = $('<table>' + tableContents + '</table>');
+    this.echo(table);
+  },
+  whoami: function() {
+    this.echo('Kevin Cao');
+  },
+  id: function() {
+    this.echo('name=1000(Kevin Cao)   major=998(Computer Science)   spec=56(Software Engineering)');
+  },
+  pwd: function() {
+    this.echo('/undergrad/Indiana_University/Bloomington');
+  },
+  ps: function() {
+    const table = $('<table>'+
+            '<tr>' +
+              '<td style="width: 1em; text-align: right;">PID</td>' +
+              '<td style="width: 3em;">TTY</td>' +
+              '<td style="width: 6em; text-align: right;">TIME</td>' +
+              '<td>CMD</td>' +
+            '</tr>' +
+            '<tr>' +
+              '<td style="text-align: right;">1</td>' +
+              '<td>?</td>' +
+              '<td style="text-align: right;">2020-Now</td>' +
+              '<td>IUSG Project Manager</td>' +
+            '</tr>' +
+            '<tr>' +
+              '<td style="text-align: right;">2</td>' +
+              '<td>?</td>' +
+              '<td style="text-align: right;">2019-Now</td>' +
+              '<td>IU Undergraduate Instructor</td>' +
+            '</tr>' +
+            '<tr>' +
+              '<td style="text-align: right;">3</td>' +
+              '<td>?</td>' +
+              '<td style="text-align: right;">2019-2020</td>' +
+              '<td>IU SICE Tutor</td>' +
+            '</tr>' +
+            '<tr>' +
+              '<td style="text-align: right;">4</td>' +
+              '<td>?</td>' +
+              '<td style="text-align: right;">2015-Now</td>' +
+              '<td>Tokyo Grill IT Support</td>' +
+            '</tr>' +
+          '</table>');
+
+    this.echo(table);
+  },
+  locale: function() {
+    this.echo('Java\nC\nPython\nRacket\nJavascript\nC#\nGDScript\nSwift\nKotlin');
+  },
+  ls: function() {
+    for (let name in projects) {
+      let anchor = $(`<a href=${projects[name]} style='color: var(--classy-blue)' class='nodecor' target='_blank'>${name}</a>`);
+      this.echo(anchor);
+    }
+  }
+}, {
+  prompt: '[[;#97ff00;]> ]',
+  greetings: "Type 'help' for a list of commands.",
+  checkArity: true,
+  height: 600,
+  onAfterCommand: function(command) {
+    if (command === 'clear') {
+      console.log($);
+      this.echo("Type 'help' for a list of commands.");
+    }
+  },
+  keydown: function(e, term) {
+    if (e.ctrlKey) {
+      return true;
+    }
+  }
+});
+
+
